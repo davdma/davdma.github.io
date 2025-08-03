93dbb468c037b2267d8ebdd149fa7174a9bca387
davdma <42689743+davdma@users.noreply.github.com>
2025-08-03 04:46:54 +0000

    Deploying to gh-pages from @ davdma/davdma.github.io@73b119ed94f817afbfb866414b20b2e6ecfc6899 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,7 +23,18 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/cv/";
           },
-        },{id: "post-learning-git",
+        },{id: "post-the-tmux-tool",
+      
+        title: "The Tmux Tool",
+      
+      description: "Teaching myself to use the terminal multiplexer",
+      section: "Posts",
+      handler: () => {
+        
+          window.location.href = "/blog/2025/Learning-Tmux/";
+        
+      },
+    },{id: "post-learning-git",
       
         title: "Learning Git",
       
